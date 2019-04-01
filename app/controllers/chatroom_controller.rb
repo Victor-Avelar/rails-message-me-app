f73ea3516fafbe066e22503ab44d58876e86b97f
Victor-Avelar <victor.r.avelar@gmail.com>
2019-04-01 16:14:01 -0700

    Change message limit to message.all (was Message.custom_display) in chatroomcontroller.rb and modified message.rb as a temporary fix for heroku redis

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,7 +2,8 @@ class ChatroomController < ApplicationController
   before_action :require_user
   def index
     @message = Message.new
-    @messages = Message.custom_display
+    # @messages = Message.custom_display
+    @messages = Message.all
     @users = User.where(is_online?: true)
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true
-  scope :custom_display, -> {order(:created_last).last(20)}
+  # scope :custom_display, -> {order(:created_last).last(20)}
 end
